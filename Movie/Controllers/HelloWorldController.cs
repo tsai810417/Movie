779756829c87392dec1e448926376d076e383c8d
tsai810417 <tsai_cheng@yahoo.com>
2018-08-03 14:50:06 +0800

    let Welcome take an id as argument

--- Movie/Controllers/HelloWorldController.cs
+++ Movie/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,18 @@ namespace Movie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        //
+        // Get: /HelloWorld/
+        public string Index()
         {
-            return View();
+            return "This is my default action";
+        }
+
+        //
+        // Get: /HelloWorld/Welcome/:id
+        public string Welcome(int ID)
+        {
+            return HtmlEncoder.Default.Encode($"Welcome, your ID is: {ID}");
         }
     }
 }
